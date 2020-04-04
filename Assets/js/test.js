101ca86cbd9af699f7efc6dd13e03c0f6efe9eca
MathurinNkenfack <mathurinnkenfack@gmail.com>
2020-04-04 19:32:00 +0100

    files

---
+++ Assets/js/test.js
@@ -0,0 +1,6 @@
+
+$("#menu a").click(function(e){
+    e.preventDefault()
+    var selText = $(this).text();
+    $(this).parents('#nav-1').find('#lan-select').html(selText+' <span class="fa fa-chevron-down" style="padding-left: 5px;"></span>');
+});
