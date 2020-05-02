2a1e2090ba84aa69f2ef633b3559d60e68df5f8a
MathurinNkenfack <mathurinnkenfack@gmail.com>
2020-05-02 12:38:59 +0100

    'almost done -- 5 pages left'

--- Assets/js/test.js
+++ Assets/js/test.js
@@ -1,6 +1,5 @@
-
-$("#menu a").click(function(e){
+$("#menu a").click(function(e) {
     e.preventDefault()
     var selText = $(this).text();
-    $(this).parents('#nav-1').find('#lan-select').html(selText+' <span class="fa fa-chevron-down" style="padding-left: 5px;"></span>');
+    $(this).parents('#nav-1').find('#lan-select').html(selText + ' <span class="fa fa-chevron-down" style="padding-left: 5px;"></span>');
 });
