f9b339666153663df9b2618266db82252d889b9b
MathurinNkenfack <mathurinnkenfack@gmail.com>
2020-05-27 07:50:02 +0100

    links

--- Assets/js/test.js
+++ Assets/js/test.js
@@ -3,3 +3,9 @@ $("#menu a").click(function(e) {
     var selText = $(this).text();
     $(this).parents('#nav-1').find('#lan-select').html(selText + ' <span class="fa fa-chevron-down" style="padding-left: 5px;"></span>');
 });
+
+$("#form-1 select:first").change(function(e) {
+    var links = ["register-1.html", "register-2.html"];
+    var index = $("#form-1 select:first option:selected").val();
+    $("#form-1").attr("action", links[index]);
+})
